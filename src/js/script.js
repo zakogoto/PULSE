de1cb5ca78801bffff0583cebf5eb9cd91f75bbf
zakogoto <nekitoss01@icloud.com>
2022-10-31 00:13:13 +0600

    made new tiny slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,62 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/icon/left_arrow.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/icon/right_arrow.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    autoplay: true,
-                    dots: true,
-                    arrows: false
-                }
-            },
-        ]
-    });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         // adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="img/icon/left_arrow.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="img/icon/right_arrow.svg"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     autoplay: true,
+//                     dots: true,
+//                     arrows: false
+//                 }
+//             },
+//         ]
+// //     });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    gutter: '20px',
+    slideBy: 1,
+    controls: false,
+    dots: false,
+    autoplay: false,
+    loop: true,
+    speed: 1200,
+    nav: false
+});
+
+document.querySelector('.prev').onclick = function () {
+    slider.goTo('prev');
+};
+
+document.querySelector('.next').onclick = function () {
+    slider.goTo('next');
+};
     
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-        $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-      });
-
-      
-    $('.catalog-item__link').each(function(i) {
-        $(this).on('click', function(e) {
-            e.preventDefault();
-            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-        });
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
-    $('.catalog-item__back').each(function(i) {
-        $(this).on('click', function(e) {
-            e.preventDefault();
-            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-        });
+    
+$('.catalog-item__link').each(function(i) {
+    $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
     });
 });
+
+$('.catalog-item__back').each(function(i) {
+    $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    });
+});
+// });
